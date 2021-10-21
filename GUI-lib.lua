9342e6618e0acdc2ec5709465b031c60fb2504d3
Schneemann <45300885+Schneemann84@users.noreply.github.com>
2021-10-20 20:13:24 -0400

    Update GUI-lib.lua

--- GUI-lib.lua
+++ GUI-lib.lua
@@ -1,6 +1,7 @@
 local component=require("component")
 local term=require("term")
 local color=require("color")
+local gpu=require("gpu")
 
 local gui={}
 
@@ -10,9 +11,30 @@ local windows = {
         ["exampleButton"] = {id = 0, func = (function() print("this is an example button"), width = 10, height = 3, locx = 3, locy = 5, text = "click", position = "centered"}
     }
 }
+    
+local test = {}
 
 function gui.createWindow(name,width,height,locx,locy,backColor,foreColor)
   
 end
+    
+function gui.createLabel(name,width,height,locx,locy,foreColor,backColor,text)
+      
+   
+    
+end
+    
+function gui.createTest(name,width,height,locx,locy,color)
+    test["name"] = name
+    test["name"]["width"] = width
+    test["name"]["height"] = height
+    test["name"]["locx"] = locx
+    test["name"]["locy"] = locy
+    test["name"]["color"] = color
+end
 
+function gui.update()
+    gpu.fill(test["name"]["locx"],test["name"]["locy"],test["name"]["width"],test["name"]["height"],test["name"]["color"])
+end
+    
 return gui
